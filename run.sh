ac99d5da97d6d1e8fb6586875c14892d6022beeb
Kevin Bennett <flightgod@gmail.com>
2022-10-18 14:45:34 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,4 @@
 git clone https://github.com/prowler-cloud/prowler
 cd prowler
 ./prowler -M csv -f us-east-1,us-west-2
-cd output
-ls
-/home/cloudshell-user/AWS/prowler/output
+cd /home/cloudshell-user/AWS/prowler/output
